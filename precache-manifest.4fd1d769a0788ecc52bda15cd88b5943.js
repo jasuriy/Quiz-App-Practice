86e15ecd5d70c24afb3e96e7242d95dd84bd8cca
jasuriy <jasur1998gopirjonov@gmail.com>
2020-07-23 11:37:25 +0500

    Updates

---
+++ precache-manifest.4fd1d769a0788ecc52bda15cd88b5943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bd5db60e00d879656ff313c8b048db",
+    "url": "/Quiz-App-Practice/index.html"
+  },
+  {
+    "revision": "73d7eff229d6699eb7c1",
+    "url": "/Quiz-App-Practice/static/css/main.8772c9f3.chunk.css"
+  },
+  {
+    "revision": "2be3614b0e84631ef5f4",
+    "url": "/Quiz-App-Practice/static/js/2.d25c553c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-App-Practice/static/js/2.d25c553c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d7eff229d6699eb7c1",
+    "url": "/Quiz-App-Practice/static/js/main.52c2beec.chunk.js"
+  },
+  {
+    "revision": "00f673a1473f9b7991f9",
+    "url": "/Quiz-App-Practice/static/js/runtime-main.90870d1c.js"
+  }
+]);
